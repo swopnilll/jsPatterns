54b8e1375bfeb42b55d6ee0809097416618b56f6
swopnilll <swopnilacharya@gmail.com>
2024-10-31 22:14:27 +1100

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,31 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let stringArray = str.trim().split("");
+
+  let maxCount = 0;
+
+  let maxOccurChar = "";
+
+  let tempCount;
+
+  for (let element of stringArray) {
+    tempCount = 0;
+
+    for (let element2 of stringArray) {
+      if (element2 === element) {
+        tempCount++;
+      }
+    }
+
+    if (tempCount > maxCount) {
+      maxCount = tempCount;
+      maxOccurChar = element;
+    }
+  }
+
+  return maxOccurChar;
+}
 
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,16 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let isNegativeNumber = Math.sign(n) === -1 ? true : false;
+
+  let updatedNumber = isNegativeNumber ? n * -1 : n;
+
+  let unsignedNumber = updatedNumber.toString().split("").reverse().join("");
+
+  return isNegativeNumber
+    ? Number(unsignedNumber) * -1
+    : Number(unsignedNumber);
+}
 
 module.exports = reverseInt;
